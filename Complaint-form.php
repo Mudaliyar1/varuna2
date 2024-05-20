1ada7462db1ba8e765f3e7e2a32e5044028eef5a
Mudaliyar1 <160152185+Mudaliyar1@users.noreply.github.com>
2024-05-21 00:09:00 +0530

    Update Complaint-form.php

--- Complaint-form.php
+++ Complaint-form.php
@@ -4,9 +4,21 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
+    <script src="https://www.google.com/recaptcha/api.js" async defer></script>
+
     <link rel="stylesheet" href="Complaint-form.css">
 </head>
 <body>
-    <p>nothing here</p>
+<form action="submit.php" method="POST">
+        <label for="name">Name:</label>
+        <input type="text" id="name" name="name" required>
+        <br>
+        <label for="email">Email:</label>
+        <input type="email" id="email" name="email" required>
+        <br>
+        <div class="g-recaptcha" data-sitekey="YOUR_SITE_KEY"></div>
+        <br>
+        <button type="submit">Submit</button>
+    </form>
 </body>
 </html>
