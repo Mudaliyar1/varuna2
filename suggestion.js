a7acaa8e196b6ae476548ce5f8f6637b9802e23f
Vijay <vijaymudaliyar224@gmail.com>
2024-10-08 15:27:57 -0700

    calc added

---
+++ suggestion.js
@@ -0,0 +1,9 @@
+function opens(){
+    let open=document.getElementById("calculator").style.display="block";
+    // let hides=document.getElementById("body").style.overflow="hidden";
+}
+function closes(){
+    let close=document.getElementById("calculator").style.display="none";
+    let hides=document.getElementById("body").style.overflow="scroll";
+
+}
