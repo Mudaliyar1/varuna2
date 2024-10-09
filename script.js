21c917b29f9ac55cbb6cc66398c9d61fd83fe2a9
Vijay <vijaymudaliyar224@gmail.com>
2024-10-08 22:44:50 -0700

    dark mode added

---
+++ script.js
@@ -0,0 +1,13 @@
+function darks(){
+    let darked_b=document.getElementById("body-main").style.backgroundColor="darkgrey";
+    let darked_c=document.getElementById("body-main").style.color="white";
+    let dayss=document.getElementById("day-icon").style.display="block";
+    let darks=document.getElementById("dark-icon").style.display="none";
+    let darks_2=document.getElementById("body-4part").style.color="black";
+}
+function dayss(){
+    let darked_b=document.getElementById("body-main").style.backgroundColor="white";
+    let darked_c=document.getElementById("body-main").style.color="black";
+    let dayss=document.getElementById("day-icon").style.display="none";
+    let darks=document.getElementById("dark-icon").style.display="block";
+}
