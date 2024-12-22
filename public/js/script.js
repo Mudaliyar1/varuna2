069a37d2362b3b0304de733a1514fe6c500911e1
Vijay <vijaymudaliyar224@gmail.com>
2024-12-22 01:23:57 -0800

    Update script.js

--- public/js/script.js
+++ public/js/script.js
@@ -1,3 +1,6 @@
+var tl=gsap.timeline();
+
+
 gsap.from('#body-txt #rd3-txt',{
     x:'-70%',
     duration:0.4,
@@ -5,9 +8,64 @@ gsap.from('#body-txt #rd3-txt',{
     scrollTrigger:{
         trigger:'#body-txt #rd3-txt',
         scroller:'body',
-        markers:true,
+        // markers:true,
         start:'top 80%',
         end:'top 70%',
         scrub:2
+    }
+});
+
+
+
+gsap.from('#body2-txt #body-item1',{
+    y:-300,
+    rotateX:'150deg',
+    rotateY:'200deg',
+    opacity:0,
+    scrollTrigger:{
+        trigger:'#body2-txt #body-item1',
+        scroller:'body',
+        markers:true,
+        start:'top 90%',
+        end:'top 70%',
+        scrub:1,
+    }
+})
+gsap.from('#body2-txt #body-item2',{
+    x:-300,
+    opacity:0,
+    rotateX:'150deg',
+    rotateY:'200deg',
+    scrollTrigger:{
+        trigger:'#body2-txt #body-item1',
+        scroller:'body',
+        markers:true,
+        start:'top 90%',
+        end:'top 90%',
+        scrub:1,
+    }
+})
+gsap.from('#body2-txt #body-item3',{
+    x:300,
+    opacity:0,
+    scrollTrigger:{
+        trigger:'#body2-txt #body-item1',
+        scroller:'body',
+        markers:true,
+        start:'top 110%',
+        end:'top 100%',
+        scrub:1,
+    }
+})
+gsap.from('#body2-txt #body-item4',{
+    y:800,
+    opacity:0,
+    scrollTrigger:{
+        trigger:'#body2-txt #body-item1',
+        scroller:'body',
+        markers:true,
+        start:'top 110%',
+        end:'top 100%',
+        scrub:1,
     }
 })
